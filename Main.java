6b7767f40efddf320351e27dde8364fbc6a960a0
Cameron Ortega <137089034+Stiver32@users.noreply.github.com>
2025-02-11 20:01:03 +0000

    change machine sucmission

--- Main.java
+++ Main.java
@@ -1,16 +1,38 @@
 // COLLABORATORS: <tutors, instructor, or anyone else who helped you>
 
-// DESCRIPTION: <a sentence about what the purpose of this program is>
+// DESCRIPTION: converts a dollar amount to quarters, dimes, nickels, and pennies. 
 
 public class Main
 {
 	public static void main(String[] args)
 	{
     //DECLARATION + INITIALIZATION SECTION
+    double dollarAmount;
+    int pennyAmount;
+    int nickelAmount;
+    int dimeAmount;
+    int quarterAmount;
+    int centsRemaining;
 
     //CALCULATION SECTION
+    dollarAmount = 1.68;
+    centsRemaining = (int) (dollarAmount * 100);
+
+    quarterAmount = (int) (centsRemaining/25);
+    centsRemaining %= 25;
+
+    dimeAmount = (int) (centsRemaining/10);
+    centsRemaining %= 10;
+
+    nickelAmount = (int) (centsRemaining/5);
+    centsRemaining %= 5;
+
+    pennyAmount = centsRemaining;
+  
 
     //OUTPUT SECTION
+
+    System.out.printf("$%.2f can be converted to %d quarters, %d dimes, %d nickels, and %d pennies.", dollarAmount,quarterAmount,dimeAmount,nickelAmount,pennyAmount);
 
   }
 
